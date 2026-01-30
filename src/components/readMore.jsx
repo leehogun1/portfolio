2c905467a823b0461390947ee46df322f4009f29
leehogun1 <leehogun1@gmail.com>
2026-01-30 22:27:48 +0900

    aboutUs 수정

--- src/components/readMore.jsx
+++ src/components/readMore.jsx
@@ -1,16 +1,13 @@
 import '../styles/readMore.css'
 import Icon from './icons/icons'
-import { Link } from 'react-router-dom'
 
 function readMore(props) {
     return (
         <>
-            <Link to={`/detail/${props.no}`}>
-                <a className='readMoreBox'>
+                <div className='readMoreBox'>
                     <Icon name='Read More' width={20} height={20} className='readMore' />
                     <Icon name='Read More Hover' width={20} height={20} className='readMore-hover' />
-                </a>
-            </Link>
+                </div>
         </>
     )
 }
--- src/sections/myProject.jsx
+++ src/sections/myProject.jsx
@@ -1,6 +1,7 @@
 import '../styles/myProject.css'
 import ReadMore from '../components/readMore'
 import projectImg1 from '../assets/project1-img.webp'
+import { Link } from 'react-router-dom'
 
 function myProject({ refProp }) {
     return (
@@ -15,12 +16,14 @@ function myProject({ refProp }) {
                         <img src={projectImg1} alt='project-1-img'></img>
                     </div>
                     <div className='project-left-contents'>
-                        <span className='display-text project-no'>01</span>
+                        {/* <span className='display-text project-no'>01</span> 나중에 추가 */}
+                        <Link to={`/detail/1`} className='goDetail'>
                         <h2 className='bold project-name'>문화유산 통합 정보 플랫폼</h2>
                         <span className="p2">문화재청 공공 API를 기반으로 문화유산 데이터를 수집·정제하여
                             지도, 게시판, 뉴스, 반환 연표 형태로 제공하는 통합 문화유산 정보 플랫폼입니다.
                             문화유산을 일상 속에서 자연스럽게 접할 수 있도록 하여, 사용자들의 관심을 높이는 것을 목표로 했습니다.</span>
-                        <ReadMore no='1'></ReadMore>
+                        <ReadMore></ReadMore>
+                        </Link>
                     </div>
                 </div>
             </div>
